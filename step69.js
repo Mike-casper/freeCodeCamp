def6a10c041117eb40344193305ab31d2fb03df7
Mike-casper <misha7709123@gmail.com>
2022-06-01 21:29:46 +0300

    Create step69.js

---
+++ step69.js
@@ -0,0 +1,16 @@
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val) {
+    return "Outside";// if (val<10 || val>20) {
+  }
+
+  if (val) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
